2f170191aa971d77fd48d18146075d620cb6e973
Pavel Solovev <p.solovev@articul.ru>
2022-11-06 16:46:08 +0400

    Basic types

---
+++ src/basic-types.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// Создаем схему, используя язык GraphQL
+const schema = buildSchema(`
+  type Query {
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
+  }
+`);
+
+// Корень предоставляет функцию разрешения для каждой конечной точки API
+
+const root = {
+  quoteOfTheDay: () => {
+    return Math.random < 0.5 ? 'Take it easy' : 'Salvation lies within';
+  },
+
+  random: () => {
+    return Math.random();
+  },
+
+  rollThreeDice: () => {
+    return [1, 2, 3].map((_) => 1 + Math.floor(Math.random() * 6));
+  },
+
+  rollDice(numDice: Int!, numSides: Int): [Int]
+};
+
+const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(4000);
+console.log(`Running a GraphQL API server at http://localhost:4000/graphql`);
--- src/post-query.js
+++ src/post-query.js
@@ -8,3 +8,23 @@ fetch('/graphql', {
 })
   .then((r) => r.json())
   .then((data) => console.log('data returned', data));
+
+var dice = 3;
+var sides = 6;
+var query = `query RollDice($dice: Int!, $sides: Int) {
+  rollDice(numDice: $dice, numSides: $sides)
+}`;
+
+fetch('/graphql', {
+  method: 'POST',
+  headers: {
+    'Content-type': 'application/json',
+    Accept: 'application/json',
+  },
+  body: JSON.stringify({
+    query,
+    variables: { dice, sides },
+  }),
+})
+  .then((r) => r.json())
+  .then((data) => console.log('data returned', data));
